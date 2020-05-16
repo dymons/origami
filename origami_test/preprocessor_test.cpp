7156e9beb3c51c95eec621dc9444f89976de7f80
Dmitriy Emelyanov <emelyanov.dv@starline.ru>
2020-05-16 18:21:06 +0300

    Добавил тесты для предпроцессора id, else, elif, endif

--- origami_test/preprocessor_test.cpp
+++ origami_test/preprocessor_test.cpp
@@ -23,6 +23,23 @@ class PreprocessorsTest : public ::testing::Test {
       });
     }
 
+    /**
+      * \brief          Подсчитывает количество token'ов, полученных после лексического анализа, указанного типа
+      *
+      * \param[in]      t_code - исходный код программы
+      * \param[in]      t_count_token - значение token'ов, количество которых необходимо посчитать после лексического анализа
+      *
+      * \return         Количество token'ов указанного типа
+      */
+    template<typename ... Tokens>
+    std::size_t countOfTokens(const std::string& t_code, Tokens ... t_count_token)
+    {
+      const auto tokens = m_tokenizer.getTokens(t_code);
+      return std::count_if(tokens.begin(), tokens.end(), [=](const auto& t_token) {
+        return ((t_token.first == t_count_token) || ...);
+      });
+    }
+
     origami::lex::LexicalConventions m_tokenizer; ///< Лексический анализатор
 };
 
@@ -119,4 +136,107 @@ TEST_F(PreprocessorsTest, Include)
                           {{ Token::KeywordPreprocessor, "#include" },
                            { Token::Punctuator,          "\"" },
                            { Token::Identifier,          "customdefine" }}));
+}
+
+TEST_F(PreprocessorsTest, If)
+{
+  using origami::lex::Token;
+//
+//  ASSERT_TRUE(equalTokens("#if", {{ Token::KeywordPreprocessor, "#if" }}));
+//  ASSERT_TRUE(equalTokens("#elif ", {{ Token::KeywordPreprocessor, "#elif" }}));
+//  ASSERT_TRUE(equalTokens("#else ", {{ Token::KeywordPreprocessor, "#else" }}));
+//  ASSERT_TRUE(equalTokens("#endif ", {{ Token::KeywordPreprocessor, "#endif" }}));
+
+  const std::string code = "#if DLEVEL > 5\n"
+                           "    #define SIGNAL  1\n"
+                           "    #if STACKUSE == 1\n"
+                           "        #define STACK   200\n"
+                           "    #else\n"
+                           "        #define STACK   100\n"
+                           "    #endif\n"
+                           "#else\n"
+                           "    #define SIGNAL  0\n"
+                           "    #if STACKUSE == 1\n"
+                           "        #define STACK   100\n"
+                           "    #else\n"
+                           "        #define STACK   50\n"
+                           "    #endif\n"
+                           "#endif\n"
+                           "#if DLEVEL == 0\n"
+                           "    #define STACK 0\n"
+                           "#elif DLEVEL == 1\n"
+                           "    #define STACK 100\n"
+                           "#elif DLEVEL > 5\n"
+                           "    display( debugptr );\n"
+                           "#else\n"
+                           "    #define STACK 200\n"
+                           "#endif";
+
+  EXPECT_EQ(countOfTokens(code, Token::KeywordPreprocessor), 23);
+  ASSERT_TRUE(equalTokens(code,
+                          {{ Token::KeywordPreprocessor, "#if" },
+                           { Token::Identifier,          "DLEVEL" },
+                           { Token::Operator,            ">" },
+                           { Token::Literal,             "5" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "SIGNAL" },
+                           { Token::Literal,             "1" },
+                           { Token::KeywordPreprocessor, "#if" },
+                           { Token::Identifier,          "STACKUSE" },
+                           { Token::Operator,            "==" },
+                           { Token::Literal,             "1" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "200" },
+                           { Token::KeywordPreprocessor, "#else" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "100" },
+                           { Token::KeywordPreprocessor, "#endif" },
+                           { Token::KeywordPreprocessor, "#else" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "SIGNAL" },
+                           { Token::Literal,             "0" },
+                           { Token::KeywordPreprocessor, "#if" },
+                           { Token::Identifier,          "STACKUSE" },
+                           { Token::Operator,            "==" },
+                           { Token::Literal,             "1" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "100" },
+                           { Token::KeywordPreprocessor, "#else" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "50" },
+                           { Token::KeywordPreprocessor, "#endif" },
+                           { Token::KeywordPreprocessor, "#endif" },
+                           { Token::KeywordPreprocessor, "#if" },
+                           { Token::Identifier,          "DLEVEL" },
+                           { Token::Operator,            "==" },
+                           { Token::Literal,             "0" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "0" },
+                           { Token::KeywordPreprocessor, "#elif" },
+                           { Token::Identifier,          "DLEVEL" },
+                           { Token::Operator,            "==" },
+                           { Token::Literal,             "1" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "100" },
+                           { Token::KeywordPreprocessor, "#elif" },
+                           { Token::Identifier,          "DLEVEL" },
+                           { Token::Operator,            ">" },
+                           { Token::Literal,             "5" },
+                           { Token::Identifier,          "display" },
+                           { Token::Punctuator,          "(" },
+                           { Token::Identifier,          "debugptr" },
+                           { Token::Punctuator,          ")" },
+                           { Token::Punctuator,          ";" },
+                           { Token::KeywordPreprocessor, "#else" },
+                           { Token::KeywordPreprocessor, "#define" },
+                           { Token::Identifier,          "STACK" },
+                           { Token::Literal,             "200" },
+                           { Token::KeywordPreprocessor, "#endif" }
+                          }));
 }
