a1d997d4553aacdb4c53b40afedfa96d13e579e8
Емельянов Дмитрий <emelyanov.dv@starline.ru>
2020-06-13 12:15:29 +0300

    Еще часть тестов

--- origami_test/operators_test.cpp
+++ origami_test/operators_test.cpp
@@ -6,7 +6,7 @@
 /**
  * \brief          Преобразует исходный код программы в последовательность token'ов и сравнивает с ожидаемыми token'ами
  *
- * \param[in]      t_tokens - последовательность лексем ло лексического анализатора исходный код программы
+ * \param[in]      t_tokens - последовательность лексем, от лексического анализатора, для исходный кода программы
  * \param[in]      t_expect_tokens - ожидаемая последовательность token'ов, которые будут получены после лексического анализа
  *
  * \retval         true - Последовательность token'ов исходного кода программы совпадает с ожидаемой последовательностью
@@ -22,7 +22,7 @@ template<typename T> bool equalTokens(const T& t_tokens, const T& t_expect_token
 /**
  * \brief          Подсчитывает количество token'ов, полученных после лексического анализа, указанного типа
  *
- * \param[in]      t_code - исходный код программы
+ * \param[in]      t_tokens - последовательность лексем, от лексического анализатора, для исходный кода программы
  * \param[in]      t_count_token - значение token'ов, количество которых необходимо посчитать после лексического анализа
  *
  * \return         Количество token'ов указанного типа
@@ -54,5 +54,103 @@ TEST_CASE("Проверка аналитического разбора прог
     REQUIRE(!equalTokens(tokenizer.getTokens("< =>"), { { Token::Operator, "<=>" } }));
     REQUIRE(!equalTokens(tokenizer.getTokens("< <"), { { Token::Operator, "<<" } }));
     REQUIRE(!equalTokens(tokenizer.getTokens("< <="), { { Token::Operator, "<<=" } }));
+
+    const std::string code =
+      "auto main() -> int\n"
+      "<%\n"
+      "    auto lambda = <:&:> { };\n"
+      "    constexpr auto a = 5;\n"
+      "    constexpr auto b = 10;\n"
+      "    static_assert(a < b);\n"
+      "    static_assert(a <= b);\n"
+      "\n"
+      "    std::deque<int> container;\n"
+      "    int value = 123;\n"
+      "    do\n"
+      "    <% \n"
+      "        container.push_front(value);\n"
+      "    %> while (value <<= 1);\n"
+      "    return 0;\n"
+      "%>";
+
+    REQUIRE(countOfTokens(tokenizer.getTokens(code), Token::Operator) == 10);
+    REQUIRE(countOfTokens(tokenizer.getTokens(code), Token::Punctuator) == 31);
+    REQUIRE(equalTokens(tokenizer.getTokens(code),
+      { { Token::Keyword, "auto" },
+        { Token::Identifier, "main" },
+        { Token::Punctuator, "(" },
+        { Token::Punctuator, ")" },
+        { Token::Punctuator, "->" },
+        { Token::Keyword, "int" },
+        { Token::Punctuator, "<%" },
+        { Token::Keyword, "auto" },
+        { Token::Identifier, "lambda" },
+        { Token::Operator, "=" },
+        { Token::Punctuator, "<:" },
+        { Token::Operator, "&" },
+        { Token::Punctuator, ":>" },
+        { Token::Punctuator, "{" },
+        { Token::Punctuator, "}" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "constexpr" },
+        { Token::Keyword, "auto" },
+        { Token::Identifier, "a" },
+        { Token::Operator, "=" },
+        { Token::Literal, "5" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "constexpr" },
+        { Token::Keyword, "auto" },
+        { Token::Identifier, "b" },
+        { Token::Operator, "=" },
+        { Token::Literal, "10" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "static_assert" },
+        { Token::Punctuator, "(" },
+        { Token::Identifier, "a" },
+        { Token::Operator, "<" },
+        { Token::Identifier, "b" },
+        { Token::Punctuator, ")" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "static_assert" },
+        { Token::Punctuator, "(" },
+        { Token::Identifier, "a" },
+        { Token::Operator, "<=" },
+        { Token::Identifier, "b" },
+        { Token::Punctuator, ")" },
+        { Token::Punctuator, ";" },
+        { Token::Identifier, "std" },
+        { Token::Punctuator, "::" },
+        { Token::Identifier, "deque" },
+        { Token::Operator, "<" },
+        { Token::Keyword, "int" },
+        { Token::Operator, ">" },
+        { Token::Identifier, "container" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "int" },
+        { Token::Identifier, "value" },
+        { Token::Operator, "=" },
+        { Token::Literal, "123" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "do" },
+        { Token::Punctuator, "<%" },
+        { Token::Identifier, "container" },
+        { Token::Punctuator, "." },
+        { Token::Identifier, "push_front" },
+        { Token::Punctuator, "(" },
+        { Token::Identifier, "value" },
+        { Token::Punctuator, ")" },
+        { Token::Punctuator, ";" },
+        { Token::Punctuator, "%>" },
+        { Token::Keyword, "while" },
+        { Token::Punctuator, "(" },
+        { Token::Identifier, "value" },
+        { Token::Operator, "<<=" },
+        { Token::Literal, "1" },
+        { Token::Punctuator, ")" },
+        { Token::Punctuator, ";" },
+        { Token::Keyword, "return" },
+        { Token::Literal, "0" },
+        { Token::Punctuator, ";" },
+        { Token::Punctuator, "%>" } }));
   }
 }
