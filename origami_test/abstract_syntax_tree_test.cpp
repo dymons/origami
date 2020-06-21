babd69a8e8e4d5fbd6a66b9bcda390b5c7d0a669
Емельянов Дмитрий <emelyanov.dv@starline.ru>
2020-06-21 19:10:46 +0300

    Добавил первый тест

---
+++ origami_test/abstract_syntax_tree_test.cpp
@@ -0,0 +1,17 @@
+#include "origami_syntax/parsing/syntax_analysis.hpp"
+
+#include <catch2/catch.hpp>
+
+#include <memory>
+
+TEST_CASE("Проверка на суммирвоание выражения Абстрактного синтаксического дерева", "[abstract-syntax-tree-sum]")
+{
+  origami::parser::AstNodeVisitor visitor;
+
+  auto sum_node = std::make_shared<origami::parser::SumNode>();
+  sum_node->addLeft(std::make_shared<origami::parser::ValueNode<int>>(10));
+  sum_node->addRight(std::make_shared<origami::parser::ValueNode<int>>(20));
+  std::optional<int> res = sum_node->accept(origami::parser::Data<int>(), visitor);
+  REQUIRE(res);
+  REQUIRE(res.value() == 30);
+}
