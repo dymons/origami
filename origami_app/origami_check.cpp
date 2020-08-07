0b51d3aecfdb86061ebf8332d15d8db20dbf556f
Емельянов Дмитрий <emelyanov.dv@starline.ru>
2020-08-07 22:09:39 +0300

    Удалил флаг подключения 11 стандарта

--- origami_app/origami_check.cpp
+++ origami_app/origami_check.cpp
@@ -1,7 +1,9 @@
 #include <clang/Frontend/TextDiagnosticBuffer.h>
+#include <clang/Tooling/CommonOptionsParser.h>
+#include <clang/Tooling/Tooling.h>
+#include <llvm/Support/CommandLine.h>
 
 int main(int argc, char** argv)
 {
-
   return 0;
 }
